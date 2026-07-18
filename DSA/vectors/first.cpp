9a72d5456ed2ff7304c51cd46e220dbb0e573d5b
KaushikPradhan789 <kaushikpradhan19@gmail.com>
2026-07-18 11:10:19 +0530

    .

--- DSA/vectors/first.cpp
+++ DSA/vectors/first.cpp
@@ -1,5 +1,6 @@
 // DYNAMICAL RESIZEABLE ARRAYS ARE CALLED VECCTORS.
 // SYNTAX WILL BE vector<datatype> vector_name; ALSO CAN BE CREATED LIKE ARRAYS vector<datatype> vector_name(size, initial_value);
+//MEMORY ALLOCATED DYNAMICALLY IN HEAP MEMORY.STATIC MEMORY IS ALLOCATED IN STACK MEMORY.
 
 #include <iostream>
 #include <vector>
@@ -15,4 +16,6 @@ int main(){
    
    v.push_back(1); // adding an element at the end of the vector
    cout << "size of the vector: "  <<v.size() << endl; // size of the vector
+
+   //v.front , v.back , v.pop_back , v.clear , v.empty , v.resize , v.swap , v.insert , v.erase , v.assign , v.at are also used for different operations on vectors.
 }
